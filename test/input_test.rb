7ac91597bc61a8d14c540f88fbe61356dda2b494
Janko Marohnić <janko.marohnic@gmail.com>
2017-03-11 17:04:29 +1100

    Add tests for Tus::Input

---
+++ test/input_test.rb
@@ -0,0 +1,46 @@
+require_relative "test_helper"
+require "stringio"
+
+describe Tus::Input do
+  before do
+    @io = StringIO.new("input")
+    @input = Tus::Input.new(@io)
+  end
+
+  it "wraps the underlying IO object" do
+    assert_equal 5, @input.size
+    refute @input.eof?
+
+    assert_equal "input", @input.read
+    assert @input.eof?
+    assert_equal "",      @input.read
+    @input.rewind
+    refute @input.eof?
+
+    assert_equal "input", @input.read(10)
+    assert @input.eof?
+    @input.rewind
+
+    assert_equal "in",  @input.read(2)
+    refute @input.eof?
+    assert_equal "put", @input.read(3)
+    assert @input.eof?
+    assert_equal nil,   @input.read(1)
+    @input.rewind
+
+    buffer = ""
+    @input.read(2, buffer)
+    assert_equal "in", buffer
+    refute @input.eof?
+    @input.read(3, buffer)
+    assert_equal "put", buffer
+    assert @input.eof?
+    @input.read(1, buffer)
+    assert_equal "", buffer
+    assert @input.eof?
+    @input.rewind
+
+    @input.close
+    refute @io.closed?
+  end
+end
